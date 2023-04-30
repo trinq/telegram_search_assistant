de9708821de551217933a1019e8c491ed26ad037
trinq <triquang13@gmail.com>
2023-04-30 15:48:38 +0700

    update

--- app/main.py
+++ app/main.py
@@ -16,6 +16,50 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup_event():
     print("FastAPI server started")
+# Import necessary modules
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI  # Imports the FastAPI web framework
+import telebot  # Imports the TeleBot package for Telegram bots
+from telebot import types  # Imports the necessary types package from TeleBot
+
+load_dotenv()  # Loads environment variables from the .env file in the project directory
+
+# Import settings and functions from other Python files
+from config.settings import TELEGRAM_API_TOKEN, OPENAI_API_KEY
+from app.chatgpt import generate_response
+
+# Define a TeleBot instance using the provided API token
+bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
+
+# Define a FastAPI instance
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    print("FastAPI server started")
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    # Extract chat id and user query from incoming telegram message
+    chat_id = message.chat.id
+    user_query = message.text
+
+    # Generate response using OpenAI's GPT-3 model
+    response = generate_response(user_query)
+
+    # Send response back to user in Telegram chat
+    bot.send_message(chat_id, response)
+
+# Define a webhook handler to receive updates from Telegram's servers
+@app.post("/webhook/{token}")
+async def webhook_handler(token: str):
+    if token != TELEGRAM_API_TOKEN:
+        return "Invalid token"
+
+    update = telebot.types.Update.de_json(await app.state.reader.read())
+    bot.process_new_updates([update])
+    return "OK"
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
